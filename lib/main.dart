fdcf46da80ceaee364aa9ea289113643e6fee473
alfred-dev21 <159748110+alfred-dev21@users.noreply.github.com>
2024-02-12 07:40:26 +0200

    added code for appearance of the app using widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,192 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
+    const String appTitle = 'Travel App';
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        body: const SingleChildScrollView(
+          child: Column(
+            children: [
+              ImageSection(image: "./images/table-mountain.jpg"),
+              TitleSection(
+                name: "Table Mountain",
+                location: "Capetown, Western Cape",
+              ),
+              ButtonSection(),
+              TextSection(
+                description:
+                    """Table Mountain, flat-topped mountain in southwestern South Africa, overlooking Cape Town and Table Bay and dominating the northern end of the high, rocky Cape Peninsula. Its tabular shape results from nearly horizontal layers of sandstone exposed by vigorous wind and water erosion.""",
+              )
+            ],
+          ),
+        ),
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class TitleSection extends StatelessWidget {
+  final String name;
+  final String location;
+
+  const TitleSection({
+    super.key,
+    required this.name,
+    required this.location,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+    return Padding(
+        padding: const EdgeInsets.all(32),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                // first column
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: Text(
+                      name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    location,
+                    style: TextStyle(color: Colors.grey[500]),
+                  ),
+                ],
+              ),
+            ),
+            Icon(
+              // third column
+              Icons.star,
+              color: Colors.red[500],
+            ),
+            const Text('62'),
+          ],
+        ));
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Theme.of(context).primaryColor;
+
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ButtonWithText(
+            color: color,
+            icon: Icons.call,
+            label: "CALL",
+          ),
+          ButtonWithText(
+            color: color,
+            icon: Icons.near_me,
+            label: "ROUTE",
+          ),
+          ButtonWithText(
+            color: color,
+            icon: Icons.share,
+            label: "SHARE",
+          ),
+        ],
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    );
+  }
+}
+
+class ButtonWithText extends StatelessWidget {
+  final Color color;
+  final IconData icon;
+  final String label;
+
+  const ButtonWithText({
+    super.key,
+    required this.color,
+    required this.icon,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              color: color,
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
         ),
+      ],
+    );
+  }
+}
+
+class TextSection extends StatelessWidget {
+  final String description;
+
+  const TextSection({
+    super.key,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        description,
+        softWrap: true,
       ),
+    );
+  }
+}
+
+class ImageSection extends StatelessWidget {
+  final String image;
+
+  const ImageSection({
+    super.key,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      image,
+      height: 240,
+      width: 600,
+      fit: BoxFit.cover,
     );
   }
 }
